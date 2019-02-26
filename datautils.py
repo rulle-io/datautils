01406533f9ae6fd6c49f307e2c9d0ca57b5094a2
Ruslan Altynnikov <ruslan@spotify.com>
2019-02-26 13:45:07 +0100

    Second version: all tasks (except start_daate/end_date) implemented.

--- datautils.py
+++ datautils.py
@@ -2,29 +2,60 @@ from datetime import datetime, timedelta
 from functools import reduce
 
 
-def hole_filter_str(data_input, dt_format ='%Y%m%d'):
-    """Takes a list of dates as strings and return a list of dates, which are the "holes".
+def hole_filter_str(data_input, dt_format ='%Y-%m-%d', is_sorted=True, units='days'):
+    """
+    Takes a list of dates as strings and return a list of dates as strings in the same format, which are the "holes".
+    Strings are converted to datetime objects using dt_format pattern.
+    :param data_input: a list of input strings
+    :param dt_format: a pattern used in datetime.strptime()
+    :param is_sorted: boolean, will trigger sort of the list if set to False
+    :param units: timedelta units. One of following [days, hours]
     """
     dates_as_dt = list(map(lambda x: datetime.strptime(x, dt_format), data_input))
-    # TODO test invalid format
-    data_filtered = hole_filter_dt(dates_as_dt)
+    data_filtered = hole_filter_dt(dates_as_dt, is_sorted, units)
     return list(map(lambda x: x.strftime(dt_format), data_filtered))
 
 
 def reduce_f(x, y):
-    d_delta = timedelta(days=1)
-    delta = (y - x[1]).days
-    #  if (y - x[1] == d_delta):
-    if (delta == 1):
-        return (x[0], y)
+    """
+    Function used in reduce method.
+    :param x: pair of (list of "hole" datetime
+    :param y: last processed "non-hole" datetime
+    :return: reduced pair (x, y)
+    """
+    dt_delta = y - x[1]
+    ext_units = x[2]  # inconvenient way to supply a parameter for a reduce function
+    # it starts to look not concise :)
+    if ext_units == "days":
+        int_units = 'days'
+        int_divisor = 1
+    else:  # hours
+        int_units = 'seconds'
+        int_divisor = 60*60  # seconds --> hours
+    # due to no attribute 'hours' on timedelta
+    units_delta = int(getattr(dt_delta, int_units) / int_divisor)
+    if units_delta < 0:
+        raise ValueError("Looks like this list is not sorted")
+
+    if units_delta == 1:
+        return x[0], y, x[2]
     else:
-        for i in range(1, delta):
-            x[0].append(x[1] + timedelta(days=i))  # mutated x[0]
-    return (x[0], y)
+        for i in range(1, units_delta):
+            x[0].append(x[1] + timedelta(**{ext_units: i}))  # mutated x[0]
+    return x[0], y, x[2]
 
 
-def hole_filter_dt(input_dates, is_sorted=True):
-    found_holes = [] # can be mutated in reduce_f
+def hole_filter_dt(input_dates, is_sorted=True, units='days'):
+    """
+    Takes a list of datetime instances and return a list of datetimes, which are the "holes" in this sequence.
+    :param input_dates: a list of input datetimes
+    :param is_sorted: boolean, will trigger sort of the list if set to False
+    :param units: timedelta units. One of following [days, seconds, microseconds, milliseconds, minutes, hours, weeks]
+    :return: a list of found "hole" datetimes
+    """
+    found_holes = []  # can be mutated in reduce_f
     if len(input_dates) > 1:
-        reduce(reduce_f, input_dates[1:], (found_holes, input_dates[0]))
+        if not is_sorted:
+            input_dates = sorted(input_dates)
+        reduce(reduce_f, input_dates[1:], (found_holes, input_dates[0], units))
     return found_holes
--- test/test_datautils.py
+++ test/test_datautils.py
@@ -6,39 +6,53 @@ from datautils import hole_filter_str
 class TestDataUtils(TestCase):
 
     def test_input_zero(self):
-        self.f_input = []
-        self.expected = []
-        self.f_output = hole_filter_str(data_input=self.f_input, dt_format ='%Y-%m-%d')
-        self.assertEqual(self.f_output, self.expected)
+        f_input = []
+        expected = []
+        f_output = hole_filter_str(data_input=f_input)
+        self.assertEqual(f_output, expected)
+
+    def test_input_invalid(self):
+        f_input = ["one", "stop", "shop"]
+        with self.assertRaises(ValueError):
+            hole_filter_str(data_input=f_input)
+
+    def test_input_unsorted(self):
+        f_input = ["2018-01-05", "2018-01-01"]
+        with self.assertRaises(ValueError):
+            hole_filter_str(data_input=f_input)
 
     def test_input_one(self):
-        self.f_input = ["2018-01-01"]
-        self.expected = []
-        self.f_output = hole_filter_str(data_input=self.f_input, dt_format ='%Y-%m-%d')
-        self.assertEqual(self.f_output, self.expected)
+        f_input = ["2018-01-01"]
+        expected = []
+        f_output = hole_filter_str(data_input=f_input)
+        self.assertEqual(f_output, expected)
 
     def test_one_hole(self):
-        self.f_input = ["2018-01-01", "2018-01-03"]
-        self.expected = ["2018-01-02"]
-        self.f_output = hole_filter_str(data_input=self.f_input, dt_format ='%Y-%m-%d')
-        self.assertEqual(self.f_output, self.expected)
+        f_input = ["2018-01-01", "2018-01-03"]
+        expected = ["2018-01-02"]
+        f_output = hole_filter_str(data_input=f_input)
+        self.assertEqual(f_output, expected)
+
+    def test_one_hole_hours(self):
+        f_input = ["2018-01-03T08", "2018-01-03T10"]
+        expected = ["2018-01-03T09"]
+        f_output = hole_filter_str(data_input=f_input, dt_format ='%Y-%m-%dT%H', units='hours')
+        self.assertEqual(f_output, expected)
+
+    def test_one_hole_unsorted(self):
+        f_input = ["2018-01-03", "2018-01-01"]
+        expected = ["2018-01-02"]
+        f_output = hole_filter_str(data_input=f_input, is_sorted=False)
+        self.assertEqual(f_output, expected)
+
+    def test_one_hole_diff_format(self):
+        f_input = ["20180101", "20180103"]
+        expected = ["20180102"]
+        f_output = hole_filter_str(data_input=f_input, dt_format ='%Y%m%d')
+        self.assertEqual(f_output, expected)
 
     def test_two_holes(self):
         f_input = ["2018-01-01", "2018-01-03", "2018-01-05"]
         expected = ["2018-01-02", "2018-01-04"]
-
-        f_output = hole_filter_str(data_input=f_input, dt_format ='%Y-%m-%d')
+        f_output = hole_filter_str(data_input=f_input)
         self.assertEqual(f_output, expected)
-
-
-
-#             output, dict) or isinstance(output, list))
-    #
-    #         task = BackfillsDump(date=datetime.today())
-    #         output = False
-    #         try:
-    #             output = task.output()
-    #         except (RuntimeError, TypeError, NameError):
-    #             pytest.fail("Output is not well behaved")
-    #         assert (isinstance(output, luigi.target.Target) or isinstance(
-    #             output, dict) or isinstance(output, list))
