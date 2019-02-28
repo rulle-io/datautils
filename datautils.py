c8a73e5e4fc1f6efbf5ee21b18fc8af5823ca748
Ruslan Altynnikov <ruslan@spotify.com>
2019-02-28 14:11:13 +0100

    Switch to use target interval and actual dates. Implemented using sets.

--- datautils.py
+++ datautils.py
@@ -1,61 +1,66 @@
 from datetime import datetime, timedelta
-from functools import reduce
 
 
-def hole_filter_str(data_input, dt_format ='%Y-%m-%d', is_sorted=True, units='days'):
+def hole_filter_str(period_start, period_end, data_input, dt_format ='%Y-%m-%d', units='days'):
     """
-    Takes a list of dates as strings and return a list of dates as strings in the same format, which are the "holes".
-    Strings are converted to datetime objects using dt_format pattern.
+    Finds 'holes' in interval [period_start .. period_end) given a list of existing dates as strings.
+    Return a list of dates as strings in the same format, which are the "holes" in the interval.
+    Strings are converted to datetime objects using 'dt_format' pattern.
+    :param period_start: start of period, inclusive
+    :param period_end: end of period, exclusive
     :param data_input: a list of input strings
     :param dt_format: a pattern used in datetime.strptime()
-    :param is_sorted: boolean, will trigger sort of the list if set to False
     :param units: timedelta units. One of following [days, hours]
+    :returns sorted list of 'holes' as string(s)
     """
-    dates_as_dt = list(map(lambda x: datetime.strptime(x, dt_format), data_input))
-    data_filtered = hole_filter_dt(dates_as_dt, is_sorted, units)
+    dates_as_dt = map(lambda x: datetime.strptime(x, dt_format), data_input)
+    period_start_dt = datetime.strptime(period_start, dt_format)
+    period_end_dt = datetime.strptime(period_end, dt_format)
+    data_filtered = hole_filter_dt(period_start_dt, period_end_dt, dates_as_dt, units)
     return list(map(lambda x: x.strftime(dt_format), data_filtered))
 
 
-def reduce_f(x, y):
-    """
-    Function used in reduce method.
-    :param x: pair of (list of "hole" datetime
-    :param y: last processed "non-hole" datetime
-    :return: reduced pair (x, y)
-    """
-    dt_delta = y - x[1]
-    ext_units = x[2]  # inconvenient way to supply a parameter for a reduce function
-    # it starts to look not concise :)
-    if ext_units == "days":
+def get_period_length_in_units(period_start, period_end, units):
+    period_delta = period_end - period_start
+    if units == "days":
         int_units = 'days'
         int_divisor = 1
-    else:  # hours
+    elif units == "hours":
         int_units = 'seconds'
-        int_divisor = 60*60  # seconds --> hours
-    # due to no attribute 'hours' on timedelta
-    units_delta = int(getattr(dt_delta, int_units) / int_divisor)
-    if units_delta < 0:
-        raise ValueError("Looks like this list is not sorted")
-
-    if units_delta == 1:
-        return x[0], y, x[2]
+        int_divisor = 60 * 60  # seconds --> hours
+        # due to no attribute 'hours' on timedelta
     else:
-        for i in range(1, units_delta):
-            x[0].append(x[1] + timedelta(**{ext_units: i}))  # mutated x[0]
-    return x[0], y, x[2]
+        raise ValueError("Unsupported unit")
+    # due to no attribute 'hours' on timedelta
+    units_delta = int(getattr(period_delta, int_units) / int_divisor)
+    return units_delta
 
 
-def hole_filter_dt(input_dates, is_sorted=True, units='days'):
+def hole_filter_dt(period_start, period_end, input_dates, units='days'):
     """
     Takes a list of datetime instances and return a list of datetimes, which are the "holes" in this sequence.
-    :param input_dates: a list of input datetimes
-    :param is_sorted: boolean, will trigger sort of the list if set to False
-    :param units: timedelta units. One of following [days, seconds, microseconds, milliseconds, minutes, hours, weeks]
-    :return: a list of found "hole" datetimes
+    :param period_start: start of period, inclusive (datetime)
+    :param period_end: end of period, exclusive (datetime)
+    :param input_dates: a list of input (as datetime)
+    :param units: timedelta units. One of following [days, hours]
+    :returns: a sorted list of found "holes" (as datetime)
     """
-    found_holes = []  # can be mutated in reduce_f
-    if len(input_dates) > 1:
-        if not is_sorted:
-            input_dates = sorted(input_dates)
-        reduce(reduce_f, input_dates[1:], (found_holes, input_dates[0], units))
+
+    full_list = generate_dt_list(period_start, period_end, units)
+    full_set = set(full_list)
+    actual_set = set(input_dates)
+    found_holes = sorted(list(full_set - actual_set))
     return found_holes
+
+
+def generate_dt_list(period_start_dt, period_end_dt, units):
+    """
+    Generates a list of datetime objects using given interval [period_start_dt .. period_end_dt)
+    :param period_start_dt: start of period, inclusive
+    :param period_end_dt: end of period, exclusive
+    :param units:
+    :returns: a generated list
+    """
+    units_number = get_period_length_in_units(period_start_dt, period_end_dt, units)
+    full_list = [period_start_dt + timedelta(**{units: i}) for i in range(0, units_number)]
+    return full_list
--- test/test_datautils.py
+++ test/test_datautils.py
@@ -1,58 +1,70 @@
+import datetime
 from unittest import TestCase
 
-from datautils import hole_filter_str
+from datautils import hole_filter_str, generate_dt_list
 
 
 class TestDataUtils(TestCase):
 
-    def test_input_zero(self):
-        f_input = []
-        expected = []
-        f_output = hole_filter_str(data_input=f_input)
+    def test_generate_date_list(self):
+        start_p = datetime.date(2018, 1, 1)
+        end_p = datetime.date(2018, 1, 3)
+        expected = [datetime.date(2018, 1, 1), datetime.date(2018, 1, 2)]
+        f_output = generate_dt_list(period_start_dt=start_p, period_end_dt=end_p, units='days')
         self.assertEqual(f_output, expected)
 
-    def test_input_invalid(self):
+    def test_input_zero_interval(self):
+        f_input = []
+        expected = []
+        f_output = hole_filter_str(period_start="2018-01-01", period_end="2018-01-01", data_input=f_input)
+        self.assertEqual(f_output, expected)
+
+    def test_input_invalid_input(self):
         f_input = ["one", "stop", "shop"]
         with self.assertRaises(ValueError):
-            hole_filter_str(data_input=f_input)
+            hole_filter_str(period_start="2018-01-01", period_end="2018-01-01", data_input=f_input)
 
-    def test_input_unsorted(self):
-        f_input = ["2018-01-05", "2018-01-01"]
+    def test_input_invalid_unit(self):
+        f_input = []
         with self.assertRaises(ValueError):
-            hole_filter_str(data_input=f_input)
+            hole_filter_str(period_start="2018-01-01", period_end="2018-01-03", data_input=f_input, units='weeks')
 
     def test_input_one(self):
+        p_start = "2018-01-01"
+        p_end = "2018-01-02"
         f_input = ["2018-01-01"]
         expected = []
-        f_output = hole_filter_str(data_input=f_input)
+        f_output = hole_filter_str(period_start=p_start, period_end=p_end, data_input=f_input)
         self.assertEqual(f_output, expected)
 
     def test_one_hole(self):
-        f_input = ["2018-01-01", "2018-01-03"]
+        p_start = "2018-01-01"
+        p_end = "2018-01-03"
+        f_input = ["2018-01-01"]
         expected = ["2018-01-02"]
-        f_output = hole_filter_str(data_input=f_input)
+        f_output = hole_filter_str(period_start=p_start, period_end=p_end, data_input=f_input)
         self.assertEqual(f_output, expected)
 
     def test_one_hole_hours(self):
-        f_input = ["2018-01-03T08", "2018-01-03T10"]
-        expected = ["2018-01-03T09"]
-        f_output = hole_filter_str(data_input=f_input, dt_format ='%Y-%m-%dT%H', units='hours')
-        self.assertEqual(f_output, expected)
-
-    def test_one_hole_unsorted(self):
-        f_input = ["2018-01-03", "2018-01-01"]
-        expected = ["2018-01-02"]
-        f_output = hole_filter_str(data_input=f_input, is_sorted=False)
+        p_start = "2018-01-01T08"
+        p_end = "2018-01-01T10"
+        f_input = ["2018-01-01T08"]
+        expected = ["2018-01-01T09"]
+        f_output = hole_filter_str(period_start=p_start, period_end=p_end, data_input=f_input, dt_format ='%Y-%m-%dT%H', units='hours')
         self.assertEqual(f_output, expected)
 
     def test_one_hole_diff_format(self):
-        f_input = ["20180101", "20180103"]
+        p_start = "20180101"
+        p_end = "20180103"
+        f_input = ["20180101"]
         expected = ["20180102"]
-        f_output = hole_filter_str(data_input=f_input, dt_format ='%Y%m%d')
+        f_output = hole_filter_str(period_start=p_start, period_end=p_end, data_input=f_input, dt_format ='%Y%m%d')
         self.assertEqual(f_output, expected)
 
     def test_two_holes(self):
-        f_input = ["2018-01-01", "2018-01-03", "2018-01-05"]
+        p_start = "2018-01-01"
+        p_end = "2018-01-05"
+        f_input = ["2018-01-01", "2018-01-03"]
         expected = ["2018-01-02", "2018-01-04"]
-        f_output = hole_filter_str(data_input=f_input)
+        f_output = hole_filter_str(period_start=p_start, period_end=p_end, data_input=f_input)
         self.assertEqual(f_output, expected)
